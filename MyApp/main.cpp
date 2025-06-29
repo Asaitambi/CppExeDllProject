d82329ee70c9a35f4e6bf332293301e7562bd780
Asaitambi <asaiit07@gmail.com>
2025-06-29 12:01:51 +0530

    changes

--- MyApp/main.cpp
+++ MyApp/main.cpp
@@ -2,7 +2,7 @@
 #include "MyLibrary.h"
 
 int main() {
-    std::cout << "Calling DLL function...how are you" << std::endl;
+    std::cout << "Calling DLL function ror sample" << std::endl;
     say_hello();
     return 0;
 }
